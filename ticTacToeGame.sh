9b7b2934b7d084ba874cb047e8db1fc47324121a
gajanan15 <gajanandesai97@gmail.com>
2020-02-03 11:02:26 +0530

    ADDED:Function To Reset Game

--- ticTacToeGame.sh
+++ ticTacToeGame.sh
@@ -1,2 +1,20 @@
 #!/bin/bash -x
 echo "Welcome To Tic Tac Toe Game"
+
+#Constants
+ROWS=3
+COLUMNS=3
+
+#Declare Array
+declare -a boardOfGame
+
+function resettingBoard() {
+	for((i=0;i<ROWS;i++))
+	do
+		for((j=0;j<COLUMNS;j++))
+		do
+			boardOfGame[$i,$j]=""
+		done
+	done
+}
+resettingBoard
