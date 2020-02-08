20e11adf23b6de6246436b0ad73abd5210c85c63
gajanan15 <gajanandesai97@gmail.com>
2020-02-08 11:22:34 +0530

    ADDED: Function To Take Available Corner If No One Wins

--- ticTacToeGame.sh
+++ ticTacToeGame.sh
@@ -61,6 +61,7 @@ function displayGameBoard() {
 	done
 }
 
+#Game Start
 function playGame() {
 	while [[ $count -lt $TOTAL_MOVE ]]
 	do
@@ -72,7 +73,7 @@ function playGame() {
 			if [[ $row -ge $ROWS || $col -ge $COLUMNS ]]
 			then
 				echo "Invalid"
-			elif [[ ${boardOfGame[$row,$col]} != $PLAYER_LETTER ]]
+			elif [[ ${boardOfGame[$row,$col]} != $PLAYER_LETTER && ${boardOfGame[$row,$col]} != $COMPUTER_LETTER ]]
 			then
 				boardOfGame[$row,$col]=$PLAYER_LETTER
 				checkForWin $PLAYER_LETTER
@@ -93,6 +94,10 @@ function playGame() {
 			then
 				computerWinningBoard $PLAYER_LETTER $COMPUTER_LETTER
 			fi
+			if [ $checkFlag -eq 0 ]
+			then
+				takingCornerPosition $COMPUTER_LETTER
+			fi
 			checkForWin $COMPUTER_LETTER
 			((count++))
 			flag=0
@@ -105,7 +110,7 @@ function checkForWin() {
 	letter=$1
 	displayGameBoard
 	winAtRowAndColumnPosition	$letter
-	winAtDiagonal $letter
+	winAtDiagonalPosition $letter
 	if [ $tieCount -gt 8 ]
 	then
 		echo "It's a Tie"
@@ -128,57 +133,61 @@ function computerWinChecking() {
 	fi
 }
 
+function reassignCounter() {
+		checkCount=0
+		newLetterCount=0
+}
+
+function checkCounterAndChangeFlagValue() {
+	if [[ $checkCount -eq 2 && $newLetterCount -eq 1 ]]
+	then
+		boardOfGame[$row,$column]=$putLetter
+		checkFlag1=1
+		checkFlag=1
+	fi
+}
+
 function computerWinningBoard() {
 	checkLetter=$1
 	putLetter=$2
 	checkFlag=0
 	checkFlag1=0
 
+	#Computer Checks All 3 Possible Winning Row Position
 	if [ $checkFlag1 -eq 0 ]
 	then
-		for((i=0;i<$ROWS;i++))
+		for((i=0;i<ROWS;i++))
 		do
-			checkCount=0
-			newLetterCount=0
-			for((j=0;j<$COLUMNS;j++))
+			reassignCounter
+			for((j=0;j<COLUMNS;j++))
 			do
 				computerWinChecking $i $j $checkLetter
 			done
-		if [[ $checkCount -eq 2 && $newLetterCount -eq 1 ]]
-		then
-			boardOfGame[$row,$column]=$putLetter
-			checkFlag1=1
-			checkFlag=1
-		fi
-	done
-	fi
-
-	if [ $checkFlag1 -eq 0 ]
-	then
-		for((i=0;i<3;i++))
-		do
-			checkCount=0
-			newLetterCount=0
-			for((j=0;j<3;j++))
-			do
-				computerWinChecking $j $i $checkLetter
-			done
-			if [[ $checkCount -eq 2 && $newLetterCount -eq 1 ]]
-			then
-				boardOfGame[$row,$column]=$putLetter
-				checkFlag1=1
-				checkFlag=1
-			fi
+			checkCounterAndChangeFlagValue $row $column $putLetter
 		done
 	fi
 
+	#Computer Checks All 3 Possible Winning Columns Position
 	if [ $checkFlag1 -eq 0 ]
 	then
-		checkCount=0
-		newLetterCount=0
-		for((i=0;i<$ROWS;i++))
+		for((i=0;i<ROWS;i++))
 		do
-			for((j=0;j<$COLUMNS;j++))
+			reassignCounter
+			for((j=0;j<COLUMNS;j++))
+			do
+				computerWinChecking $j $i $checkLetter
+			done
+			checkCounterAndChangeFlagValue $row $column $putLetter
+		done
+	fi
+
+	#Computer Check 1 Possible Winning Diagonal Position
+	if [ $checkFlag1 -eq 0 ]
+	then
+		reassignCounter
+		for((i=0;i<ROWS;i++))
+		do
+			for((j=0;j<COLUMNS;j++))
 			do
 				if [ $i -eq $j ]
 				then
@@ -186,38 +195,45 @@ function computerWinningBoard() {
 				fi
 			done
 		done
-		if [ $checkCount -eq 2 -a $newLetterCount -eq 1 ]
-		then
-			boardOfGame[$row,$column]=$putLetter
-			checkFlag1=1
-			checkFlag=1
-		fi
+		checkCounterAndChangeFlagValue $row $column $putLetter
 	fi
 
+	#Computer Check 2nd Possible Winning Diagonal Position
 	if [ $checkFlag1 -eq 0 ]
 	then
-		checkCount=0
-		newLetterCount=0
-		for((i=0;i<$ROWS;i++))
+		reassignCounter
+		for((i=0;i<ROWS;i++))
 		do
-			for((j=$((2-$i));j<$COLUMNS;j++))
+			for((j=$((2-$i));j<COLUMNS;j++))
 			do
 				computerWinChecking $i $j $checkLetter
 				break
 			done
 		done
-		if [[ $checkCount -eq 2 && $newLetterCount -eq 1 ]]
-		then
-			boardOfGame[$row,$column]=$putLetter
-			checkFlag1=1
-			checkFlag=1
-		fi
+		checkCounterAndChangeFlagValue $row $column $putLetter
 	fi
 }
 
+function takingCornerPosition(){
+	checkFlag=0
+	local putLetter=$1
+	for((i=0;i<ROWS;i=$(($i+2))))
+	do
+		for((j=0;j<COLUMNS;j=$(($j+2))))
+		do
+			if [[ ${boardOfGame[$i,$j]} == "-" ]]
+			then
+				boardOfGame[$i,$j]=$putLetter
+				checkFlag=1
+				return
+			fi
+		done
+	done
+}
+
 function playerOrComputerWon() {
-	local Letter=$1
-	if [[ $Letter == $PLAYER_LETTER ]]
+	local letter=$1
+	if [[ $letter == $PLAYER_LETTER ]]
 	then
 		echo "Player Won"
 	else
@@ -228,9 +244,9 @@ function playerOrComputerWon() {
 
 function winAtRowAndColumnPosition() {
 	letter=$1
-	for((r=0;r<$ROWS;r++))
+	for((r=0;r<ROWS;r++))
 	do
-		for((c=0;c<$COLUMNS;c++))
+		for((c=0;c<COLUMNS;c++))
 		do
 			if [[ ${boardOfGame[$r,$c]} == ${boardOfGame[$r,$(($c+1))]} ]] && [[ ${boardOfGame[$r,$(($c+1))]} == ${boardOfGame[$r,$(($c+2))]} ]] && [[ ${boardOfGame[$r,$c]} == $letter ]]
 			then
@@ -243,7 +259,7 @@ function winAtRowAndColumnPosition() {
 	done
 }
 
-function winAtDiagonal() {
+function	winAtDiagonalPosition() {
 	letter=$1
 	if [[ ${boardOfGame[0,0]} == ${boardOfGame[1,1]} ]] && [[ ${boardOfGame[1,1]} == ${boardOfGame[2,2]} ]] && [[ ${boardOfGame[0,0]} == $letter ]]
 	then
